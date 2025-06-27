6104c41cdaed611a0a635238b76dac0f9b30e354
lfourage <ludogriph@gmail.com>
2025-06-27 17:38:05 +0200

    seems done

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const upload = multer({ storage: multer.memoryStorage() });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) return res.status(400).send("No file sent");
 
+  return res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
